763e735f6f7eb593823ffd0765510c7e307cac90
Anatoly Zavarukhin <35347611+Innsmouth-trip@users.noreply.github.com>
2020-09-18 12:43:05 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# очистка сесии модх и преобразование типа таблицы 
+# очистка сессии модх и преобразование типа таблицы 
 set -e
 create_query="$(mysql $@ -e 'show create table modx_session' |cat | sed '1d; s/InnoDB/MyISAM/')"
 mysql $@ -e "drop table modx_session; $create_query;"
